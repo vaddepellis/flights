ece8ad2c3a9dbefbf1a3f80ec0d68f1f1175b742
vaddepellis <sankumar2910@gmail.com>
2025-10-09 12:14:35 +0530

    currency

--- database/migrations/2025_10_09_064042_create_currencies_table.php
+++ database/migrations/2025_10_09_064042_create_currencies_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('curency_name');
             $table->string('curency_symbol');
             $table->string('curency_code');
+            $table->string('exchange_rate');
             $table->timestamps();
         });
     }
